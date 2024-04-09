ed7f543e3bd73186b52f88fe9db7cac4028ea54d
VasanthaKumar <114223795+vasanthvk7806@users.noreply.github.com>
2024-04-09 16:22:48 +0530

    Update connectDatabase.js

--- BACKEND/config/connectDatabase.js
+++ BACKEND/config/connectDatabase.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connctDatabase = () => {
      mongoose.connect(process.env.DB_URL).then((con) => {
-        console.log('MongoDb Connected to host:'+con.connection.host)
+        
      })
 
 
